7a4b964c24fcd5ef1e07ee13d44897e2b9746250
Miguel de Icaza <miguel@MonoMac2008.local>
2009-12-05 16:55:53 -0500

    Added

---
+++ GoogleAnalytics/GANTracker.cs
@@ -0,0 +1,47 @@
+//
+// Binding to the GANTracker.h Google Analytics iPhone SDK from Google
+//
+// MIT X11 licensed
+//
+// Copyright 2009 Novell, Inc.
+//
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.ObjCRuntime;
+
+namespace GoogleAnalytics {
+
+	public partial class GANTracker {
+
+		public bool TrackPageView (string url, out NSError nsError)
+		{
+			unsafe {
+				IntPtr error;
+				IntPtr ptr_to_error = (IntPtr) (&error);
+
+				var ret = _TrackPageView (url, (IntPtr) ptr_to_error);
+				if (ptr_to_error != IntPtr.Zero)
+					nsError = (NSError) Runtime.GetNSObject (error);
+				else
+					nsError = null;
+				return ret;
+			}
+		}
+
+		public bool TrackEvent (string category, string action, string label, int value, out NSError nsError)
+		{
+			unsafe {
+				IntPtr error;
+				IntPtr ptr_to_error = (IntPtr) (&error);
+
+				var ret = _TrackEvent (category, action, label, value, (IntPtr) ptr_to_error);
+				if (ptr_to_error != IntPtr.Zero)
+					nsError = (NSError) Runtime.GetNSObject (error);
+				else
+					nsError = null;
+				return ret;
+			}
+		}
+		
+	}
+}
---
+++ GoogleAnalytics/api.cs
@@ -0,0 +1,40 @@
+//
+// Binding to the GANTracker.h Google Analytics iPhone SDK from Google
+//
+// MIT X11 licensed
+//
+// Copyright 2009 Novell, Inc.
+//
+using System;
+using MonoTouch.Foundation;
+
+namespace GoogleAnalytics {
+
+	[BaseType (typeof (NSObject))]
+	interface GANTracker {
+		[Static][Export ("sharedTracker")]
+		GANTracker SharedTracker { get; }
+
+		[Export ("startTrackerWithAccountID:dispatchPeriod:delegate:")]
+		void StartTracker (string accountiD, int dispatchPeriod, GANTrackerDelegate ganDelegate);
+
+		[Export ("stopTracker")]
+		void StopTracker ();
+
+		[Export ("trackPageview:withError:"), Internal]
+		bool _TrackPageView (string pageUrl, IntPtr nsErrorPtr);
+
+		[Export ("trackEvent:action:label:value:withError:"), Internal]
+		bool _TrackEvent (string category, string action, string label, int value, IntPtr nsErrorPtr);
+
+		[Export ("dispatch")]
+		bool Dispatch ();
+	}
+
+	[BaseType (typeof (NSObject))]
+	interface GANTrackerDelegate {
+		[Export ("trackerDispatchDidComplete:eventsDispatched:eventsFailedDispatch:")]
+		void DispatchCompleted (GANTracker tracker, int eventsDispatched, int eventsFailedDispatch);
+	}
+	
+}
